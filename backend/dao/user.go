90b869821b0ca982b84a764b3ef5142c478544ee
1695064014@qq.com <1695064014@qq.com>
2021-03-30 01:21:40 +0800

    增加用户接口开发

--- backend/dao/user.go
+++ backend/dao/user.go
@@ -1,6 +1,10 @@
 package dao
 
-import "time"
+import (
+	"log"
+	"time"
+)
+import "yxcblog/dbInit"
 
 type User struct{
 	ID int
@@ -14,4 +18,35 @@ type User struct{
 	Introducer int
 	Email string
 	LastLogin time.Time
+}
+
+func AddUser(user User)error{
+	db := dbInit.DB
+	result, err := db.Exec("INSERT INTO user (add_time, power, username, nickname, password, label_id, mobile, introducer, email, last_login) VALUES (?,1000,?,'',?,?,?,?,?,?)",
+		user.AddTime,
+		user.Username,
+		user.Password,
+		user.LabelId,
+		user.Mobile,
+		user.Introducer,
+		user.Email,
+		user.LastLogin,
+		)
+	if err!=nil{
+		return err
+	}
+	log.Println(result)
+	return nil
+}
+func QueryByUserId(){
+
+}
+func QueryByUserUserName(){
+
+}
+func QueryByUserEmail(){
+
+}
+func QueryByUserMobile(){
+
 }
